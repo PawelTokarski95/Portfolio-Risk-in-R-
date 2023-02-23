87559fcc5aec2faced0ac5c77fa914d67b031a1a
Paweł <80154676+PawelTokarski95@users.noreply.github.com>
2023-02-23 20:23:21 +0100

    Update Code.R

--- Code.R
+++ Code.R
@@ -1,22 +1,22 @@
 library(PortfolioAnalytics)
 library(quantmod)
-library(stats)
 library(PerformanceAnalytics)
 library(ggplot2)
 
+
 # Define function to calculate volatility of a stock
 Volatility_stock_market <- function(x) {
-  rR = diff(x)
-  return(abs(mean(rR, na.rm=TRUE))/sd(rR, na.rm=TRUE))
-} 
+rR = diff(x)
+return(abs(mean(rR, na.rm=TRUE))/sd(rR, na.rm=TRUE))
+}
 
 # Define ETF and stock tickers
 stock_ETF <- c('XWD.TO', 'EEM')
 
+
 # Get data for ETFs
-i = 1
 for(i in 1:length(stock_ETF)) {
-  as.data.frame(getSymbols(stock_ETF[[i]], src='yahoo', from=Sys.Date()-365*3, to=Sys.Date()))
+getSymbols(stock_ETF[[i]], src='yahoo', from=Sys.Date()-365*3, to=Sys.Date())
 }
 
 
@@ -29,36 +29,22 @@ Volatility_stock_market(EEM$EEM.Close)
 stock_tickers <- c("META", "MSFT", "GOOGL", "WMT", "AMZN", "TSLA", "XOM", "KO", "ORCL", "DIS", "NKE", "ADBE", "MA")
 
 # Get data for stocks
-i = 1
+Stocks <- data.frame()
 for(i in 1:length(stock_tickers)) {
-  as.data.frame(getSymbols.yahoo(stock_tickers[[i]],  env=globalenv(), from=Sys.Date()-365*3, to=Sys.Date()), periodicity='weekly')
+Stocks[[i]] <- getSymbols.yahoo(stock_tickers[[i]], env=globalenv(), from=Sys.Date()-365*3, to=Sys.Date(), periodicity='weekly')[,4]
 }
-
-Stocks <- data.frame(META$META.Close, MSFT$MSFT.Close, GOOGL$GOOGL.Close, WMT$WMT.Close, AMZN$AMZN.Close, TSLA$TSLA.Close, XOM$XOM.Close, KO$KO.Close, ORCL$ORCL.Close, DIS$DIS.Close, NKE$NKE.Close, ADBE$ADBE.Close, MA$MA.Close)
-Stocks_xts <- xts(Stocks, order.by = as.Date(rownames(Stocks)))
-
-
-Stocks_weekly <- data.frame()
-Stocks_weekly <- lapply(Stocks_xts, to.weekly)
-i=1
-for(i in 1:length(stock_tickers)) {
-  Stocks_weekly[[i]] <- Stocks_weekly[[i]][,4]
-}
-Stocks_weekly <- do.call(cbind, Stocks_weekly)
-colnames(Stocks_weekly) <- stock_tickers
+colnames(Stocks) <- stock_tickers
 
 # Calculate volatility for stocks
-volatilities <- c()
-i = 1
-for(i in 1:length(stock_tickers)) {
-  volatilities[i] <- Volatility_stock_market(Stocks[,i])
-}
+volatilities <- sapply(Stocks, Volatility_stock_market)
+
 
 # Calculate return on investment (ROI)
-rR <- na.omit(ROC(Stocks_weekly))
+rR <- na.omit(ROC(to.weekly(as.xts(Stocks))))
 
 # Create portfolio with stock names
 portf <- portfolio.spec(colnames(rR))
+
 
 # Add constraints to portfolio
 portf <- add.constraint(portf, type = 'weight_sum', min_sum=1, max_sum=1)
